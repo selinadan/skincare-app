99e531be52b277d52add146738fc637fe1a26e1f
team-cf-2021 <58486724+selinadan@users.noreply.github.com>
2024-07-01 14:31:19 -0700

    PROJ-1: install some libs

---
+++ src/utils/const.ts
@@ -0,0 +1,11 @@
+export const OILY = 'oily';
+export const NORMAL = 'normal';
+export const DRY = 'dry';
+export const COMBO = 'combo';
+export const SENSITIVE = ' sensitive';
+
+export const ACNE_PRONE = 'acneProne';
+export const TEXTURE = 'texture';
+export const AGING = 'aging';
+export const BRIGHTENING = 'brightening';
+export const DARK_SPOTS = 'darkSpots';

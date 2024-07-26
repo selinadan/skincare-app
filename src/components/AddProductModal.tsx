eda121aecd32bd60748e18e5ad1f0511feb2f2da
team-cf-2021 <58486724+selinadan@users.noreply.github.com>
2024-07-26 00:44:00 -0700

    PROJ-1: fix modal

--- src/components/AddProductModal.tsx
+++ src/components/AddProductModal.tsx
@@ -12,25 +12,34 @@ interface AddProductModalProps {
 	handleShowModal: (shouldShow: boolean) => void;
 }
 
+const style = {
+	position: 'absolute',
+	top: '50%',
+	left: '50%',
+	transform: 'translate(-50%, -50%)',
+	width: 400,
+	bgcolor: 'background.paper',
+	border: '2px solid #000',
+	boxShadow: 24,
+	p: 4,
+};
+
 export default function AddProductModal({
 	showModal,
-	handleShowModal
+	handleShowModal,
 }: AddProductModalProps) {
 	return (
 		<div>
-			<Modal
-				open={showModal}
-				onClose={() => handleShowModal(false)}
-			>
-				<Grid container>
+			<Modal open={showModal} onClose={() => handleShowModal(false)}>
+				<Grid container sx={style}>
 					<Grid item>
-						<TextField>
-
-						</TextField>
+						<TextField></TextField>
 					</Grid>
-					<Button onClick={() => {
-						handleShowModal(false)
-					}}>
+					<Button
+						onClick={() => {
+							handleShowModal(false);
+						}}
+					>
 						{translations.addProduct}
 					</Button>
 				</Grid>

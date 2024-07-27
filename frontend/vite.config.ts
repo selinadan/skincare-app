ba3d530001300d256fd0bfcf7e1e2dd4af7a9d6e
team-cf-2021 <58486724+selinadan@users.noreply.github.com>
2024-07-27 15:22:11 -0700

    PROJ-1: fix frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,26 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 
 export default defineConfig({
 	plugins: [react()],
-	root: './src',
-	publicDir: '../public',
+	root: '.',
+	publicDir: 'public',
 	build: {
 		outDir: '../build',
 		emptyOutDir: true,
 		rollupOptions: {
-			input: resolve(__dirname, './src/index.tsx'),
+			input: {
+				main: resolve(__dirname, 'index.html'),
+			},
 		},
 	},
 	resolve: {
 		alias: {
-			// '@': resolve(__dirname, './src'),
-			Components: resolve(__dirname, './src/components'),
-			Api: resolve(__dirname, './src/api'),
-			Utils: resolve(__dirname, './src/utils'),
+			// '@': resolve(__dirname, 'src'),
+			Components: resolve(__dirname, 'src/components'),
+			Api: resolve(__dirname, 'src/api'),
+			Utils: resolve(__dirname, 'src/utils'),
 		},
 	},
 });

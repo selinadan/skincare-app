9841d3501005f3cabfd6fea3f1b7ef22b43d45c3
team-cf-2021 <58486724+selinadan@users.noreply.github.com>
2024-07-27 19:08:56 -0700

    PROJ-1: fix buttons

--- frontend/src/components/Inventory/InventoryTable.tsx
+++ frontend/src/components/Inventory/InventoryTable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -7,15 +7,15 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
 import Rating from '@mui/material/Rating';
-import TextField from '@mui/material/TextField';
 
-import Clear from '@mui/icons-material/Clear';
+import Delete from '@mui/icons-material/Delete';
+import Edit from '@mui/icons-material/Edit';
 
 import { translations } from 'Utils/translations';
 import { Product } from 'Utils/types';
-import { getAllProducts } from 'Api/productsClient';
+import { getAllProducts, deleteProduct } from 'Api/productsClient';
 
 export default function InventoryTable() {
 	const [products, setProducts] = useState<Product[]>([]);
@@ -36,12 +36,13 @@ export default function InventoryTable() {
 		fetchProducts();
 	}, []);
 
+	const handleDelete = (id: number) => deleteProduct(id);
+
 	return (
 		<TableContainer component={Paper}>
 			<Table sx={{ minWidth: 650 }} aria-label="simple table">
 				<TableHead>
 					<TableRow>
-						<TableCell align="right">Step</TableCell>
 						<TableCell>{translations.name}</TableCell>
 						<TableCell>{translations.category}</TableCell>
 						<TableCell>{translations.price}</TableCell>
@@ -59,37 +60,21 @@ export default function InventoryTable() {
 									},
 								}}
 							>
-								<TableCell
-									component="th"
-									scope="row"
-									align="right"
-								>
-									{index}
+								<TableCell>
+									{product.name}
 								</TableCell>
-								<TableCell component="th" scope="row">
-									<TextField
-										id="name"
-										label="Outlined"
-										variant="standard"
-									>
-										{product.name}
-									</TextField>
-								</TableCell>
+								<TableCell>{product.category}</TableCell>
 								<TableCell>{product.price}</TableCell>
-								<TextField
-									id="price"
-									label="Outlined"
-									variant="standard"
-								>
-									{product.price}
-								</TextField>
 								<TableCell>
 									<Rating />
 								</TableCell>
-								<TableCell>
-									<Button color="error">
-										<Clear />
-									</Button>
+								<TableCell align='right'>
+									<IconButton>
+										<Edit />
+									</IconButton>
+									<IconButton color="error" onClick={() => handleDelete(product.id)}>
+										<Delete />
+									</IconButton>
 								</TableCell>
 							</TableRow>
 						))}
